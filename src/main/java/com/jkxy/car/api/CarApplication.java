2c3a005d19303d35621f7f77bc08d4f1fba79b37
ss7eby <Xinxiang1_Wei@saic-gm.com>
2023-10-24 18:00:58 +0800

    222

--- src/main/java/com/jkxy/car/api/CarApplication.java
+++ src/main/java/com/jkxy/car/api/CarApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
 public class CarApplication {
-
+    //llll
     public static void main(String[] args) {
         SpringApplication.run(CarApplication.class, args);
     }
